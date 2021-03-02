7dbe6145d618a2a5368a782faae50953dfdf2677
Dawid <chcezostacprogramista@gmail.com>
2021-03-02 19:03:46 +0100

    Aplikacja HelloWorld

---
+++ AplikacjaHelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AplikacjaHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mam na imię Dawid");
+        }
+    }
+}
